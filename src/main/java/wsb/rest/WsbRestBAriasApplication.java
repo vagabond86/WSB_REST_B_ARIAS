afcb8f55b373a8bc818dabd7c384fd12c1d57cf9
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-03-11 14:36:17 +0100

    init

---
+++ src/main/java/wsb/rest/WsbRestBAriasApplication.java
@@ -0,0 +1,13 @@
+package wsb.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbRestBAriasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbRestBAriasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/rest/WsbRestBAriasApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbRestBAriasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
