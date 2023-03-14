e89d6e50ab95515bb48756c0aeba5b16e21975c6
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-03-14 10:03:55 +0100

    - response entity / status 200 i status 404.

--- src/main/java/wsb/rest/controllers/BaritoneAriasController.java
+++ src/main/java/wsb/rest/controllers/BaritoneAriasController.java
@@ -1,5 +1,7 @@
 package wsb.rest.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +25,13 @@ public class BaritoneAriasController {
         return baritoneAriasServices.findAll();
     }
     @GetMapping("/arias/{id}")
-    BaritoneArias findAria(@PathVariable Long id){
-        return baritoneAriasServices.find(id);
+    ResponseEntity findAria(@PathVariable Long id){
+        BaritoneArias baritoneArias = baritoneAriasServices.find(id);
+        if (baritoneArias != null){
+            return ResponseEntity.ok(baritoneArias);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 
